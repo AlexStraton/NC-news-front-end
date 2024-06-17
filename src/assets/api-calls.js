a3370f86a35dfdfe990c22a7df54a62e05375725
Alex Straton <alexandrastraton1@gmail.com>
2024-06-17 14:28:53 +0100

    set up components folders and API calls

---
+++ src/assets/api-calls.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const itemsAPI = axios.create({
+  baseURL: "https://nc-news-project-1-zm9p.onrender.com/api",
+});
+
+export function getAllArticles() {
+  return itemsAPI.get("/articles").then(({ data }) => {
+    return data.categories;
+  });
+}
