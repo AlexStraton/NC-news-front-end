eac2cb6f74641da5b18d3367b9dc972565041326
Alex Straton <alexandrastraton1@gmail.com>
2024-06-21 13:57:00 +0100

    user can delete the comment they have written

--- src/components/Comments/DeleteComment.jsx
+++ src/components/Comments/DeleteComment.jsx
@@ -3,32 +3,53 @@ import Button from "react-bootstrap/Button";
 import { UserContext } from "../Users/UserContext";
 import { useContext } from "react";
 import { useState } from "react";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Toast from "react-bootstrap/Toast";
 
 function DeleteComment(props) {
   const { comment_id, onDelete, comment_author } = props;
   const { user } = useContext(UserContext);
-  const [loading, isLoading] = useState(true);
   const [err, setError] = useState(false);
 
   const handleDelete = () => {
-    if (user === comment_author) {
-      deleteComment(comment_id).then((response) => {
-        isLoading(true);
-        onDelete(comment_id);
-        isLoading(false);
-      });
-    }
+    deleteComment(comment_id);
+    onDelete(comment_id);
   };
 
-  if (loading) {
-    return <p className='loading'>Your comment is being loaded...</p>;
-  }
+  const [show, setShow] = useState(false);
+  const toast = (
+    <Row>
+      <Col xs={6}>
+        <Toast onClose={() => setShow(false)} show={show} delay={4000} autohide>
+          <Toast.Header>
+            <small>1 min ago</small>
+          </Toast.Header>
+          <Toast.Body className='toast_body'>
+            {err
+              ? "Your comment has not been deleted sucessfully. Please try again! "
+              : "Your comment has been deleted sucessfully!"}
+          </Toast.Body>
+        </Toast>
+      </Col>
+    </Row>
+  );
 
   return (
-    <Button variant='primary' size='lg' onClick={handleDelete} type='submit'>
-      {" "}
-      Delete comment
-    </Button>
+    <div>
+      {show ? toast : null}
+
+      <Button
+        variant='primary'
+        size='lg'
+        onClick={() => {
+          setShow(true);
+          handleDelete;
+        }}
+        type='submit'>
+        Delete comment
+      </Button>
+    </div>
   );
 }
 
